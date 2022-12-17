1821eb8a1751274aa55f305255b3ea0db22860df
Evian Apollonio <evian.apollonio@gmail.com>
2022-12-17 21:42:11 +0100

    Missing icons

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 mkdir -p vendor
 aria2c -i vendor.txt -d vendor/ --auto-file-renaming=false --allow-overwrite=true
 
-inkscape --export-type=png assets/marker/*.svg
+mkdir -p assets/marker/{tmp,optimized}
+cp assets/marker/*.* assets/marker/tmp/
+inkscape --export-type=png assets/marker/tmp/*.svg
 
-magick mogrify -resize 64x64 -path assets/marker/optimized/ -format webp assets/marker/*.png
+magick mogrify -resize 32x32 -path assets/marker/optimized/ -format webp assets/marker/tmp/*.png
+
+rm -rf assets/marker/tmp/
